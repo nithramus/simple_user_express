3be81993158b47ec73adedf1605e5db211cbce46
edwin <batiste.andre53@hotmail.fr>
2019-01-23 12:10:04 +0100

    ok

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const base_router = require('./src/user');
 
-function userCreation(knex, params) {
-    return new base_router(knex, params)
+function userCreation(knex, fileUtils, params) {
+    return new base_router(knex, fileUtils, params)
 }
 
 module.exports = userCreation
